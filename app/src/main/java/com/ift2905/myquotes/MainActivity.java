c5c4cb3b715428b22f732e2f2fbcc493b54e2bdd
chumjo <jonathan.larose.fu@gmail.com>
2018-03-22 13:32:05 -0400

    Comment test for branching

--- app/src/main/java/com/ift2905/myquotes/MainActivity.java
+++ app/src/main/java/com/ift2905/myquotes/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // This is the main activity
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

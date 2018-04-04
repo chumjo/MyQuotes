8522d2820b387cfc38f7f20a14122cc8441df287
chumjo <jonathan.larose.fu@gmail.com>
2018-04-04 16:41:46 -0400

    fragment batit avec les quotes de la liste

--- app/src/main/java/com/ift2905/myquotes/MainActivity.java
+++ app/src/main/java/com/ift2905/myquotes/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
@@ -34,15 +35,13 @@ public class MainActivity extends AppCompatActivity {
         }
         // TMP END
 
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mQuoteFragmentPagerAdapter = new QuoteFragmentPagerAdapter(getSupportFragmentManager(), mNumberOfFragment);
+        mQuoteFragmentPagerAdapter = new QuoteFragmentPagerAdapter(getSupportFragmentManager(), mNumberOfFragment, mRandomQuoteArrayList);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
--- app/src/main/java/com/ift2905/myquotes/QuoteFragmentPagerAdapter.java
+++ app/src/main/java/com/ift2905/myquotes/QuoteFragmentPagerAdapter.java
@@ -3,20 +3,25 @@ package com.ift2905.myquotes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class QuoteFragmentPagerAdapter extends FragmentPagerAdapter {
 
     private final int numberOfFragment;
+    private ArrayList<Quote> mQuoteList;
 
-    public QuoteFragmentPagerAdapter(FragmentManager fm, int numberOfFragment) {
+    public QuoteFragmentPagerAdapter(FragmentManager fm, int numberOfFragment, ArrayList<Quote> quoteArrayList) {
         super(fm);
         this.numberOfFragment = numberOfFragment;
+        this.mQuoteList = quoteArrayList;
     }
 
     @Override
     public Fragment getItem(int position) {
 
-        return QuoteFragment.newInstance(position + 1);
+        return QuoteFragment.newInstance(position + 1, mQuoteList.get(position+1));
     }
 
     @Override

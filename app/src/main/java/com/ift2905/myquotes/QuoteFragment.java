affede5fa8911493f33bdb0998863059aabe48d4
chumjo <jonathan.larose.fu@gmail.com>
2018-04-07 11:26:44 -0400

    favorite listener wip

--- app/src/main/java/com/ift2905/myquotes/QuoteFragment.java
+++ app/src/main/java/com/ift2905/myquotes/QuoteFragment.java
@@ -5,11 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class QuoteFragment extends Fragment {
 
     public Quote quote;
+    public CheckBox chk_favorite;
 
     public QuoteFragment() {
         super();
@@ -31,6 +35,22 @@ public class QuoteFragment extends Fragment {
         TextView textViewAuthor = (TextView) rootView.findViewById(R.id.author);
         textViewQuote.setText(quote.getQuote());
         textViewAuthor.setText(quote.getAuthor());
+
+        chk_favorite = (CheckBox) rootView.findViewById(R.id.chk_favorite);
+
+        chk_favorite.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if(b){
+                    // Adds the quote to the favorite database
+                }
+                else {
+                    // Removes the quote to the favorite database
+                    
+                }
+            }
+        });
 
         return rootView;
     }

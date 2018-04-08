2b139460a4a58040f88f0268273a67808644f134
Unknown <dennisoro@hotmail.com>
2018-04-08 11:27:49 -0400

    ajout notification

---
+++ app/src/main/java/com/ift2905/myquotes/AlarmeReceiver.java
@@ -0,0 +1,69 @@
+package com.ift2905.myquotes;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.os.Build;
+import android.app.NotificationChannel;
+import static android.app.NotificationManager.IMPORTANCE_DEFAULT;
+
+/*
+- Classe por la gestion de notifications de l'application
+- Affiche une quote avec auteur dans la notification
+ */
+
+public class AlarmeReceiver extends BroadcastReceiver {
+    private static final String CHANNEL_ID = "com.singhajit.notificationDemo.channelId";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Quote quote_notification = RandomQuoteInitialList.getRandomQuoteFromIntialList(MainActivity.preferences);
+
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+
+        stackBuilder.addNextIntent(notificationIntent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(100,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        Notification notification = builder.setContentTitle("My Quotes notification")
+                .setContentText(quote_notification.getQuote() + "\n" + quote_notification.getAuthor())
+                .setTicker("new message from My Quotes")
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.icon_management)
+                .setContentIntent(pendingIntent).build();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(CHANNEL_ID);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Notification MyQuotes",
+                    IMPORTANCE_DEFAULT
+            );
+
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        if (notificationManager != null) {
+            notificationManager.notify(0,notification);
+        }
+
+    }
+}
--- app/src/main/java/com/ift2905/myquotes/MainActivity.java
+++ app/src/main/java/com/ift2905/myquotes/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ift2905.myquotes;
 
 import android.annotation.SuppressLint;
+import android.app.AlarmManager;
 import android.app.FragmentTransaction;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -16,14 +18,13 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
-import android.view.View;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import static com.ift2905.myquotes.R.id.container;
 
@@ -146,6 +147,18 @@ public class MainActivity extends AppCompatActivity
                 Log.d("LOL", "Came here from settting");
                 goSetting();
             }
+        }
+
+        //-- NOTIFICATIONS --//
+        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.SECOND,4);
+
+        Intent intent = new Intent(this,AlarmeReceiver.class);
+        PendingIntent broadcast = PendingIntent.getBroadcast(this,100,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        if (alarmManager != null) {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),broadcast);
         }
     }
 
